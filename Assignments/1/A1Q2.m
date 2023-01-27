49ceca18d48b1d994a39926661f0e905cf494fe3
2nav <n-raj@outlook.com>
2023-01-27 13:55:31 +0530

    modified `A2Q2` by removing division

--- Assignments/1/A1Q2.m
+++ Assignments/1/A1Q2.m
@@ -1,17 +1,19 @@
 %{
 (a)
 x = 1/a is equivalent to
-=> ax = 1
-=> ax-1 = 0
+=> a - 1/x = 0
+Let f(x) be a - 1/x, then f'(x) = 1/x^2 and f(x) / f'(x) = ax^2 - x
 %}
 
 a = input("");
-func = @(x) a*x - 1;
-deriv = @(x) a;
-start = 0;
+func = @(x) a - (1/x);
+deriv = @(x) 1/(x^2);
+divisionFunc = @(x) a*(x^2) - x;
+start = 10^-100;
 e = 0.0001;
-
-root = Newton(func,deriv,start,e);
+format long;
+root = reciprocalNewton(divisionFunc,start,e);
+format long;
 disp("a) x = 1/a = "+root);
 
 
@@ -38,14 +40,25 @@ disp("b) Numbers are: ");
 disp(root);
 disp(20 - root);
 
+function root = reciprocalNewton(divisionFunc,start,e)
+root = 0;
+last_root = start;
 
+while true
+    root = last_root - divisionFunc(last_root);
+    if abs(root-last_root)/root<e 
+        break
+    end
+    last_root = root;
+end
+end
 function root = Newton(func,deriv,start,e)
 root = 0;
 last_root = start;
 
 while true
     root = last_root - func(last_root)/deriv(last_root);
-    if abs(root-last_root)<e 
+    if abs(root-last_root)/root<e 
         break
     end
     last_root = root;
