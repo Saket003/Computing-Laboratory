a3ee7c8e5bb96523b499e5c725912516f4b3faa3
2nav <n-raj@outlook.com>
2023-04-18 22:52:49 +0530

    improved `A3Q4` by adding `bs` to adaptive method

--- Assignments/3/A3_Q4_2021MT10240_2021MT60265.m
+++ Assignments/3/A3_Q4_2021MT10240_2021MT60265.m
@@ -3,9 +3,19 @@ format long;
 f = @(x)sin(x)/x;
 a = 0;
 b = 10;
-tol = 1e-9;
-area = Traps(f,a,b,1000);
+tol = 1e-6;
+n = 1;
+
+while true
+    area = Traps(f,a,b,n);
+    if abs((past_area-area)/area) < tol
+        break;
+    end
+    past_area = area;
+    n = n+1;
+end
 disp(area);
+disp(n);
 
 function area = Traps(f,a,b,n)
 delx = (b-a)/n;
